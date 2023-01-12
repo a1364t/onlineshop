52451d7b9d9d426977323b61491212abb41d5063
Alireza <ali1985reza@gmail.com>
2023-01-12 18:48:45 +1100

    add and delete to shopping cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,8 +1,7 @@
 
-
-
 class Cart:
     def __init__(self, request):
+        """ initialise the cart """
         self.request = request
 
         self.session = request.session
@@ -13,3 +12,25 @@ class Cart:
             cart = self.session['cart'] = {}
 
         self.cart = cart
+
+    def add(self, product, quantity=1):
+        """ Add the specified product to the cart if it exists """
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+        self.save()
+
+    def remove(self, product):
+        """ Remove a product from the cart """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def save(self):
+        """ Mark session as modified save changes """
+        self.session.modified = True
