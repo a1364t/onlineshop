6949e39c5863a9773686201d14bbc35a221b85fe
Alireza <ali1985reza@gmail.com>
2023-01-12 16:09:01 +1100

    add StackedInline to admin pannel

--- products/admin.py
+++ products/admin.py
@@ -3,9 +3,19 @@ from django.contrib import admin
 from .models import Product, Comment
 
 
+class CommentsInline(admin.StackedInline):
+    model = Comment
+    fields = ['author', 'body', 'stars', 'active', ]
+    extra = 0
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', ]
+
+    inlines = [
+        CommentsInline,
+    ]
 
 
 @admin.register(Comment)
