5c41b98c1bd2f8f502eb465844f1d952c7c6b54f
Alireza <ali1985reza@gmail.com>
2023-01-14 11:28:48 +1100

    add intcomma

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'django.contrib.humanize',
 
     # third party apps
     'jalali_date',
