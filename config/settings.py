5ff46356101cf7e4592160100a73640829677eab
Alireza <ali1985reza@gmail.com>
2023-01-20 15:26:17 +1100

    n

--- config/settings.py
+++ config/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-# LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'fa'
 
 LANGUAGES = (
     ('en', 'English'),
