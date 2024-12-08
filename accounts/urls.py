aa90326319be1b13939e9699fccdee323d3d7321
Alireza <ali1985reza@gmail.com>
2024-12-08 13:47:23 +1100

    password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,40 +1,24 @@
-from allauth.account.views import ConfirmEmailView, PasswordResetFromKeyDoneView
 from django.urls import path
-from .views import CustomPasswordResetFromKeyView
-from . import views
-
+from allauth.account.views import ConfirmEmailView, PasswordResetFromKeyDoneView
+from .views import CustomPasswordResetFromKeyView, CustomPasswordChangeView, profile
 
 urlpatterns = [
-    # other URLs
+    # Email confirmation URL
     path('accounts/confirm-email/<str:key>/',
          ConfirmEmailView.as_view(), name='account_confirm_email'),
-    path('profile/', views.profile, name='profile'),
-    path(
-        "password/reset/key/<uidb36>-<key>/",
-        CustomPasswordResetFromKeyView.as_view(),
-        name="account_reset_password_from_key",
-    ),
-    path(
-        "password/reset/key/done/",
-        PasswordResetFromKeyDoneView.as_view(),
-        name="account_reset_password_from_key_done",
-    ),
 
+    # Profile URL
+    path('profile/', profile, name='profile'),
+
+    # Password reset using key
+    path("password/reset/key/<uidb36>-<key>/",
+         CustomPasswordResetFromKeyView.as_view(), name="account_reset_password_from_key"),
+
+    # Password reset done view
+    path("password/reset/key/done/", PasswordResetFromKeyDoneView.as_view(),
+         name="account_reset_password_from_key_done"),
+
+    # Change password URL
+    path('change-password/', CustomPasswordChangeView.as_view(
+        template_name='account/password_change_form.html'), name='account_change_password'),
 ]
-
-# urlpatterns.extend(
-#     [
-#         # password reset
-
-#         re_path(
-#             r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
-#             views.password_reset_from_key,
-#             name="account_reset_password_from_key",
-#         ),
-#         path(
-#             "password/reset/key/done/",
-#             views.password_reset_from_key_done,
-#             name="account_reset_password_from_key_done",
-#         ),
-#     ]
-# )
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import CustomUserChangeForm
 from orders.models import Order
-from allauth.account.views import PasswordResetFromKeyView
+from allauth.account.views import PasswordResetFromKeyView, PasswordChangeView
+from django.urls import reverse_lazy
 
 
 @login_required
@@ -12,6 +14,9 @@ def profile(request):
             request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
+            # Success message
+            messages.success(
+                request, "Your changes have been saved successfully!")
             return redirect('profile')
     else:
         form = CustomUserChangeForm(instance=request.user)
@@ -20,12 +25,19 @@ def profile(request):
     orders = Order.objects.filter(user=request.user).prefetch_related(
         'items').order_by("-datetime_created")
 
-    # total_price = orders.get_total_price()
     return render(request, 'account/profile.html', {
         'form': form,
         'orders': orders,
-        # 'total_price': total_price,
     })
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'account/password_change_form.html'
+    # Redirect to the profile page after successful password change
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
 
 
 class CustomPasswordResetFromKeyView(PasswordResetFromKeyView):
